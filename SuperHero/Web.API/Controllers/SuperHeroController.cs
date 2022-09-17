06f65ba46c942002050aa481201fd7c12120697b
Eric Saboia <saboia.eric@gmail.com>
2022-09-17 18:16:26 -0300

    feat: add post super hero

--- SuperHero/Web.API/Controllers/SuperHeroController.cs
+++ SuperHero/Web.API/Controllers/SuperHeroController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
 using Web.API.Models;
 
 namespace Web.API.Controllers
@@ -8,8 +9,16 @@ namespace Web.API.Controllers
     [Produces("application/json")]
     public class SuperHeroController : ControllerBase
     {
+        private static List<SuperHero> _herous = new List<SuperHero>
+        {
+            new SuperHero{ Id = 1, Name = "Spider Man", FirstName = "Peter", LastName = "Parker", Place = "New York City"},
+
+            new SuperHero{ Id = 2, Name = "Batman", FirstName = "Bruce", LastName = "Wayne", Place = "Gotham City"}
+        };
+
+
         /// <summary>
-        /// Retuns all super heroes
+        /// Retuns all super heroes a
         /// </summary>
         /// <returns>
         /// <response code="200">Successful operation</response>
@@ -17,19 +26,21 @@ namespace Web.API.Controllers
         /// <response code="500">Server side found a error</response>
         /// </returns>
         [HttpGet]
-        [ProducesResponseType(typeof(IEnumerable<SuperHero>),StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(IEnumerable<SuperHero>), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> Get()
         {
-            var heroes = new List<SuperHero>
+            return Ok(_herous);
+        }
 
-            {
-                new SuperHero{ Id = 1, Name = "Spider Man", FirstName = "Peter", LastName = "Parker", Place = "New York City"},
-                new SuperHero{ Id = 2, Name = "Batman", FirstName = "Bruce", LastName = "Wayne", Place = "Gotham City"}
-            };
-
-            return Ok(heroes);
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Post(SuperHero hero)
+        {
+            _herous.Add(hero);
+            return Created($"/{hero.Id} info", hero);
         }
     }
 }
