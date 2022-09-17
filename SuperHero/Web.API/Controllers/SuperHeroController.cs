ed5f5b66d747ee202c1e29857b8199439fa7f025
Eric Saboia <saboia.eric@gmail.com>
2022-09-17 14:20:40 -0300

    feat: add super hero controller

---
+++ SuperHero/Web.API/Controllers/SuperHeroController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Web.API.Models;
+
+namespace Web.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]       
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Get()
+        {
+            var heroes = new List<SuperHero>
+            {
+                new SuperHero{ Id = 1, Name = "Spider Man", FirstName = "Peter", LastName = "Parker", Place = "New York City"}
+            };
+
+            return Ok(heroes);
+        }
+    }
+}
